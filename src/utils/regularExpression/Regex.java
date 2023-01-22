3a6a61978e3b1fddfd7df51c3e003146e6269f6a
주형돈 <hdju@rntime.com>
2023-01-22 13:48:36 +0900

    정규식

---
+++ src/utils/regularExpression/Regex.java
@@ -0,0 +1,25 @@
+package utils.regularExpression;
+
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    // java.util.regex.Pattern 
+    // 정규식 문법은 Pattern api 문서 참조
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("a*b"); // a로 시작해서 b로 끝나는 패턴 
+        Matcher m = p.matcher("aaaaab"); // 패턴에 "aaaaab"문자열을 체크한다고 등록
+        boolean result = m.matches();  // 결과출력 
+        System.out.println(result);   
+
+        // 아래처럼 사용해도 됨.
+        boolean result2 = Pattern.matches("a*b", "aaaaab");
+        System.out.println(result2);
+
+        // 함수형 프로그래밍
+        Predicate<String> matchTest = p.asMatchPredicate();
+        boolean result3 = matchTest.test("aaaabcc");
+        System.out.println(result3);
+    }
+}
